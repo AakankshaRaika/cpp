12a6c13cd1be4b00e2cc90446f96bc2b762d6bd4
brijeshb42 <brijeshb42@gmail.com>
2013-07-28 15:41:51 +0530

    message

--- CPP/Testing/main.cpp
+++ CPP/Testing/main.cpp
@@ -6,94 +6,15 @@
  */
 
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <cmath>
+#include <climits>
+
 using namespace std;
 
-struct pos{
-    int i,j;
-    pos(void){
-    }
-    pos(int x,int y){
-        i = x, j = y;
-    }
-};
-
-/*
- * 
- */
-char maze[51][51],current;
-bool v[51][51];
-int h,w,count;
-vector<pos> p;
-
-void initialize(){
-    p.clear();
-    count = 0;
-    for(int i=0;i<h;i++){
-        for(int j=0;j<w;j++){
-            v[i][j] = false;
-            pos po;
-            po.i = i, po.j = j;
-            if(maze[i][j]=='A')
-                p.push_back(po);
-        }
-    }
-    current = 'A';
-}
-
-bool isLimit(int i,int j){
-    if(i>=0 && j<h && j>=0 && j<w)
-        return true;
-    return false;
-}
-
-void bfs(int x,int y){
-    cout<<".."<<x<<" "<<y<<endl;
-    queue<pos> q;
-    pos po;
-    po.i = x, po.j = y;
-    q.push(po);
-    v[po.i][po.j] = true;
-    int c;
-    while(!q.empty()){
-        pos pt = q.front();
-        q.pop();
-        for(int i=pt.i-1;i<=pt.i+1;i++){
-            for(int j=pt.j-1;j<=pt.j+1;j++){
-                if(!v[i][j] && isLimit(i,j) && maze[i][j]==maze[pt.i][pt.j]+1){
-                    q.push(new pos(i,j));
-                    cout<<i<<" "<<j<<endl;
-                }
-            }
-        }
-    }
-}
-
-void travel(){
-    for(vector<pos>::iterator it = p.begin();it!=p.end();++it){
-        if(!v[it->i][it->j])
-            bfs(it->i,it->j);
-    }
-}
-
 int main(int argc, char** argv) {
-    
-    while(cin>>h>>w && h!=0 && w!=0){
-        initialize();
-        for(int i=0;i<h;i++)
-            cin>>maze[i];
-        for(int i=0;i<h;i++){
-            for(int j=0;j<w;j++){
-                pos po;
-                po.i = i,po.j = j;
-                if(maze[i][j]=='A')
-                    p.push_back(po);
-            }
-        }
-        travel();
-    }
-    
+    int hist[] =  {6, 1, 5, 4, 5, 2, 6};
+    int n = sizeof(hist)/sizeof(hist[0]);
+    cout << "Maximum area is " << getMaxArea(hist, n);
     return 0;
 }
 
---
+++ histogram_max_area.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+int getMid(int s,int e){
+	return (s+(e-s)/2);
+}
+
+int max(int a,int b,int c){
+	return max(max(a,b),c);
+}
+
+int minVal(int *arr,int i,int j){
+    if(i==-1) return j;
+    if(j==-1) return i;
+	return (arr[i]<arr[j])?i:j;
+}
+
+int RMQUtil(int *arr,int *st,int ss,int se,int qs,int qe,int index){
+	//cout<<"rmqu\n";
+	if(qs<=ss && qe>=se)
+		return st[index];
+	if(se<qs || ss>qe)
+		return -1;
+	int m = getMid(ss,se);
+	return minVal(arr,RMQUtil(arr,st,ss,m,qs,qe,2*index+1),RMQUtil(arr,st,m+1,se,qs,qe,2*index+2));
+}
+
+int RMQ(int *arr,int *st,int n,int qs,int qe){
+    //cout<<"rmq\n";
+	if(qs<0 || qe>n-1 || qs>qe)
+		return -1;
+	return RMQUtil(arr,st,0,n-1,qs,qe,0);
+}
+
+int constructSTUtil(int *arr,int *st,int ss,int se,int index){
+	//cout<<"cStu\n";
+	if(ss==se){
+		st[index] = ss;
+		return ss;
+	}
+	int m = getMid(ss,se);
+	st[index] = minVal(arr,constructSTUtil(arr,st,ss,m,2*index+1),constructSTUtil(arr,st,m+1,se,2*index+2));
+	return st[index];
+}
+
+int *constructST(int *arr,int n){
+    //cout<<"cSt\n";
+	int x = (int)(ceil(log2(n))); //Height of segment tree
+	int max_size = 2*(int)pow((float)2, x) - 1;
+	int *st = new int[max_size];
+	constructSTUtil(arr,st,0,n-1,0);
+	return st;
+}
+
+int getMaxAreaRec(int *arr,int *st,int n,int l,int r){
+    //cout << "maxRec\n";
+	if(l>r)
+		return -1;
+	if(l==r)
+		return arr[l];
+	int m = RMQ(arr,st,n,l,r);
+	return max(getMaxAreaRec(arr,st,n,l,m-1),getMaxAreaRec(arr,st,n,m+1,r),(r-l+1)*arr[m]);
+}
+
+int getMaxArea(int *hist, int n){
+    //cout << "getMAx\n";
+	int *st = constructST(hist,n);
+	return getMaxAreaRec(hist,st,n,0,n-1);
+}
+
+int main(){
+	int hist[] =  {6, 1, 5, 4, 5, 2, 6};
+	int n = sizeof(hist)/sizeof(hist[0]);
+	//cout<<"main\n";
+	cout << "Maximum area is " << getMaxArea(hist, n);
+	return 0;
+}
